ad85eb09dadfdc7115702e6304396a42577a56f2
Gopal S <gopalcad01@brightskills.com>
2024-01-05 15:37:07 +0530

    added html and js filein javascriptdemo

---
+++ js/script.js
@@ -0,0 +1,28 @@
+alert("welcome to javascript");
+console.log("welcome to javascript");
+console.log(10);
+console.log(20.5);
+console.log({fname:"chandran",gender:"male"});
+console.table({fname:"chandran",gender:"male"});
+console.log([15,20,25,30,]);
+console.table([15,20,25,30,]);
+console.log(true);
+var a=15;
+let b=20;
+const c=25;
+console.log(a);
+console.log(b);
+console.log(c);
+
+a=30;
+b=40;
+console.log(a+   typeof a);
+console.log(b+   typeof b);
+a=([100,110,130,140]);
+console.log(a);
+console.table(a);
+
+b=1000;
+console.log(b+ typeof b);
+
+
