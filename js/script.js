58648e1a4bdeb2f56fe1a6939b10c6ce5eb569c6
Gopal S <gopalcad01@brightskills.com>
2024-01-05 17:59:43 +0530

    update js file

--- js/script.js
+++ js/script.js
@@ -22,7 +22,58 @@ a=([100,110,130,140]);
 console.log(a);
 console.table(a);
 
-b=1000;
+b=15;
 console.log(b+ typeof b);
+
+
+
+//operators
+
+// arithmatic operators
+
+var a=10;
+console.log(a+b); 
+console.log(a-b);
+console.log(a*b);
+console.log(a**b); 
+console.log(a/b);
+console.log(a%b);
+console.log(++a);
+console.log(--a); 
+
+// assign operators
+v1=20;
+console.log(v1);
+v1+=25;
+console.log(v1);
+v1-=25;
+console.log(v1);
+v1*=25;
+console.log(v1);
+v1/=25;
+console.log(v1);
+v1%=25;
+console.log(v1);
+
+//cmparision operator
+ v1=10;
+ console.log(v1==10);
+ console.log(v1==="10", typeof v1);
+
+console.log(v1!=10);
+ console.log(v1!=="10", typeof v1);
+
+ // relational operator
+ console.log(v1>12);
+ console.log(v1<12);
+ console.log(v1>=10);
+ console.log(v1<=12);
+ console.log(v1<=2);
+
+ //logical operator
+
+console.log(v1==10 || 10>4 || 10<17);
+console.log (v1!==10, typeof v1);
+
 
 
